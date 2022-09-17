e43dedda9c6cd1b37ad7f9b5060ff5d3d16a7ca3
BMRQUEZ <alemarquez1996@gmail.com>
2022-09-17 17:31:00 -0300

    Desafio final Js

--- javascrip.js
+++ javascrip.js
@@ -11,18 +11,18 @@ class Productos {
     this.precio = precio;
   }
 }
-const producto1  = new Productos (0 ,"", "Selectora Rapida",  "Selectora Rapida Falcon Chevy Torino", 46000);
-const producto3  = new Productos (3 ,"", "Selectora Rapida", "Selectora rapida para caja lancia Fiat", 30000);
+const producto1  = new Productos (0 ,"./Img/Selectora rapida 2.png", "Selectora Rapida",  "Selectora Rapida Falcon Chevy Torino", 46000);
+const producto3  = new Productos (1 ,"/Img/Selectorarapida2.png", "Selectora Rapida", "Selectora rapida para caja lancia Fiat", 30000);
 const producto2  = new Productos (2 ,"",  "Selectora Rapida", "Selectora rapida para vw Senda-Gol", 38000);
-const producto4  = new Productos (4 ,"", "Selectora Rapida", "Selectora Rapida Caja A230 Dodge Gtx Rt Polara", 47000);
-const producto5  = new Productos (5 ,"", "Volante de competicion", "Volante de competicion tapizado gamuza 355 Mm", 12000);
-const producto6  = new Productos (6 ,"", "Volante de competicion", "Volante de competicion aluminio perforado 355mm", 9000);
-const producto7  = new Productos (7 ,"", "Volante de competicion", "Volante de competicion Base Plana", 26000);
-const producto8  = new Productos (8 ,"", "Volante de competicion", "Volante de competicion tapizado Cuero 355mm", 17000);
-const producto9  = new Productos (9 ,"", "Butaca de competicion", "Butaca Nick De Competición Modelo Carrera Iv Negra Grande L", 28290);
-const producto10 = new Productos (10 ,"", "Butaca de competicion", "Butaca Nick De Competición Modelo Indy Negra Talle Grande", 32200);
-const producto11 = new Productos (11 ,"", "Butaca de competicion", "Butaca Competición Mec Sport De Calle Roja Talle Grande", 29000);
-const producto12 = new Productos (12 ,"", "Butaca de competicion", "Butaca Competición Mec Sport Cerrada Roja", 34000);
+const producto4  = new Productos (3 ,"", "Selectora Rapida", "Selectora Rapida Caja A230 Dodge Gtx Rt Polara", 47000);
+const producto5  = new Productos (4,"", "Volante de competicion", "Volante de competicion tapizado gamuza 355 Mm", 12000);
+const producto6  = new Productos (5 ,"", "Volante de competicion", "Volante de competicion aluminio perforado 355mm", 9000);
+const producto7  = new Productos (6 ,"", "Volante de competicion", "Volante de competicion Base Plana", 26000);
+const producto8  = new Productos (7 ,"", "Volante de competicion", "Volante de competicion tapizado Cuero 355mm", 17000);
+const producto9  = new Productos (8 ,"", "Butaca de competicion", "Butaca Nick De Competición Modelo Carrera Iv Negra Grande L", 28290);
+const producto10 = new Productos (9 ,"", "Butaca de competicion", "Butaca Nick De Competición Modelo Indy Negra Talle Grande", 32200);
+const producto11 = new Productos (10 ,"", "Butaca de competicion", "Butaca Competición Mec Sport De Calle Roja Talle Grande", 29000);
+const producto12 = new Productos (11 ,"", "Butaca de competicion", "Butaca Competición Mec Sport Cerrada Roja", 34000);
 
 let productos = [producto1,producto2,producto3,producto4,producto5,producto6,producto7,producto8,producto9,producto10,producto11,producto12]
 let productosEnCarrito = []
@@ -52,7 +52,7 @@ nuevoProducto.innerHTML =
                 <div class="row row -cols-4" style="row-gap: 25px ;">
                 <div class="col">
                   <div id="${producto.id}" class="card" style="width: 15rem;">
-                  <img src="../Img/Selectora rapida 2.png" class="card-img-top" alt="Selectora rapida">
+                  <img scr="${producto.imagen}" class="card-img-top" alt=${producto.nombre}>
                   <div class="card-body">
                     <hr>
                     <h5 class="card-title">${producto.nombre}</h5>
@@ -86,12 +86,13 @@ function cargarProductosCarrito(productosDelStorage) {
   productosDelStorage.forEach((productoCarrito) => {
       modalBody.innerHTML += 
       `<div class="card border-primary mb-3" id ="productoCarrito${productoCarrito.id}" style="max-width: 540px;">
-              <img class="card-img-top" src="${productoCarrito.imagen}" alt="${productoCarrito.nombre}">
+              <img class="card-img-top" scr="${productoCarrito.imagen}" alt="${productoCarrito.nombre}">
               <div class="card-body">
                       <h4 class="card-title">${productoCarrito.descripcion}</h4>
                   
-                      <p class="card-text">$${productoCarrito.precio}</p> 
-                      <button class= "btn btn-danger" id="botonEliminar"><i class="fas fa-trash-alt"></i></button>
+                      <p class="card-text" style= "font-size: 30px">$${productoCarrito.precio}  </p> 
+                      <button class= "btn btn-danger" id="botonEliminar"><i class="fas fa-trash-alt"></i>
+                      <img src="../Img/delete.png" height="30px" width="30px"  alt="delete">                      </button>
               </div>    
           
           
@@ -111,9 +112,9 @@ function compraTotal(productosTotal) {
     })
     
     if(acumulador == 0){
-        parrafoCompra.innerHTML = `<p> No hay productos en el carrito </p>`
+        parrafoCompra.innerHTML = `<p style=  "font-size: 20px"> <b>  No hay productos en el carrito </b> </p>`
     }else{
-        parrafoCompra.innerHTML = `Suma total de sus productos ${acumulador}`
+        parrafoCompra.innerHTML = `<p style= "font-size: 20px"> <b> Suma total de sus productos ${acumulador} </b> <p>`
     }}  
 
 
